5048768a2dc6fd15667582eeedbcbc129d323cb0
David Alvarez <david.alvgar23@gmail.com>
2023-04-01 23:16:56 -0700

    Termine routes, agrege services y controllers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
-const routerApi = require('./src/routes/main.routes');
+const routerApi = require('./routes/main.routes');
 
 app.use(express.json());
 app.listen(port, () => {
